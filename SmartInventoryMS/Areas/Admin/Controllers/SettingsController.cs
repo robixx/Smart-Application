41c5be1df3dae0a75c9df4d93b16a9832fb2ba2e
Shoriful Islam Robi <robi@itcbd.com>
2026-03-12 15:24:32 +0600

    Main model class

---
+++ SmartInventoryMS/Areas/Admin/Controllers/SettingsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartInventoryMS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SettingsController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult GetUserList()
+        {
+            return View();
+        }
+    }
+}
